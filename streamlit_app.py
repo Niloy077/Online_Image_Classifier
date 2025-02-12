8f3864883ea544fd3c53330af8e73648d2593801
niloy <bniloy960@gmail.com>
2025-02-12 23:02:35 +0600

    all files now gets converted to RGB

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.write("Upload an image to classify using a pre-trained ResNet18 model.")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
     
     # Preprocess image and make prediction
